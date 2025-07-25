4d2531ffc80a1b99b72f890216592471d7066672
maame-adwoa <ijels18@gmail.com>
2025-07-25 02:51:59 +0000

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+let t;
+let city ='tema';
+let age = 12;
+let istall = false;
+
+console.log("t is:", typeof t);
+console.log("city is:", typeof city);
+console.log("age is:", typeof age)
+console.log("istall is:", typeof tall)
+
+let v, w, x, y;
+
+ firstname = "Jennifer",
+ lastname ="Annan";
+ age = "12";
+let ismarried = "false";
+
+/*let firstname = "Jennifer"
+let lastname ="Annan"
+let maritalstatus = "true"
+let country = "togo"
+let age = "11*/
+
+//let firstname = "Jennifer", let lastname ="Annan", let maritalstatus = "true", let country = "togo", let age = "11"
+
+let myAge = 11;
+let yourAge = 12;
+console.log(myAge);
+console.log(yourAge);
---
+++ variable.js
@@ -0,0 +1,17 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable
+easy to reuse abd informative */
+
+let b;
+let xy = null
+firstname = "Jennifer",
+ lastname ="Annan";
+ age = "12";
+let issingle = "false";
+
+
+
+
+
